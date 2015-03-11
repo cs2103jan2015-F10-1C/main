1084b01bbdb95c8b4ffa4a32d782a5920c1fdd59
Qiu Junbin <junbin@live.com>
2015-03-11 13:54:57 +0800

    Edited the help functions in lieu with the updates for delete,edit,search functions. Found out problems with the display functions. Require attention to solve it.

--- WiseManager/WiseManager.cpp
+++ WiseManager/WiseManager.cpp
@@ -1474,36 +1474,42 @@ string WiseManager::help(string desireCommand){
 	if (desireCommand == "add"){
 
 		oss << "To use the add function, it should be input in the following format: "			<< "\r\n"
+			<< "\r\n"
 			<< "1. add <details of the task> date time -priority				or" << "\r\n"
 			<< "2. add <details of the task> time date -priority				or" << "\r\n" << "\r\n"
 			<< "An example of a simple add task input will be: "		<< "\r\n"
-			<< "add do homework 3 march 5pm -high"						<< "\r\n"
+			<< "1.	add do homework 3 march 5pm -high"						<< "\r\n"
 			<< "this means adding the task do homework on 3 march at 5pm with high priority"	<< "\r\n"
 			<< "\r\n"
 			<< "1.1	Possible flexicommands variations for adding a task will be: "					<< "\r\n"
 			<< "\r\n"
 			<< "For the inclusion of time, indicate with :, am or pm."							<< "\r\n"
-			<< "there should be no space between time and am / pm"								<< "\r\n"
-			<< " e.g.have dinner 5pm"															<< "\r\n"
-			<< " The following inputs are wrong: have dinner 5 pm OR have dinner 5 : 00"		<< "\r\n"
+			<< "there should be no space between time and am / pm (e.g.have dinner 5pm) "		<< "\r\n"
+			<< "The following inputs are wrong : have dinner 5 pm OR have dinner 5 : 00"		<< "\r\n"
 			<< "\r\n"
 
 			<< "1.2	For the inclusion of date, do note that the inputs are in (dd/mm)"				<< "\r\n"
 			<< "\r\n"
-			<< "3 mar, 3 march, mar 3, march 3, today, tomorrow, days of week(monday - sunday)" << "\r\n"
+			<< "1. 3 mar" << "\r\n"
+			<< "2. 3 march" << "\r\n"
+			<< "3. mar 3" << "\r\n"
+			<< "4. today" << "\r\n"
+			<< "5. tomorrow" << "\r\n"
+			<< "6. days of week(monday - sunday)" << "\r\n"
 			<< "\r\n"
 
 			<< "1.3	For the priority of the task, it can be varied according with inputs: "			<< "\r\n"
 			<< "\r\n"
-			<< "-high represents task of high priority."										<< "\r\n"
-			<< "-mid represents task of medium priority."										<< "\r\n"
-			<< "-low represents task of low priority."											<< "\r\n"
+			<< "1. -high represents task of high priority."											<< "\r\n"
+			<< "2. -mid represents task of medium priority."										<< "\r\n"
+			<< "3. -low represents task of low priority."											<< "\r\n"
 			<< "\r\n"
 
 			<< "1.4	For details with conflicting terms such as certain days in a week and am / pm, user needs to include a quotation mark" << "\r\n"
 			<< "\r\n"
+			<< "To avoid creating a task on tuesday or at 3pm."									<< "\r\n"
 			<< "e.g.meet at \"taco tuesday\" or meet at \"cafe 3pm\" or \"high tea\""			<< "\r\n"
-			<< "this is to avoid creating a task on tuesday or at 3pm."							<< "\r\n";
+			<< "\r\n";
 	}
 	else if (desireCommand == "display"){
 
@@ -1525,24 +1531,25 @@ string WiseManager::help(string desireCommand){
 		oss << "To use the search function, it should be input in the following format: "		<< "\r\n"
 			<< "search <task description>"														<< "\r\n"
 			<< "the function will then return the tasks with the keywords input by the user. "	<< "\r\n"
-			<< "please take note the the task indexes as they will be used for delete and edit purposes" << "\r\n"
+			<< "please take note the the task indexes (6 number) as they will be used for delete and edit purposes" << "\r\n"
 			<< "\r\n";
 	}
 	else if (desireCommand == "delete"){
 
 		oss << "To use the delete function, it should be input in the following format: "	<< "\r\n"
 			<< "delete <task index>"														<< "\r\n"
-			<< "the function will then delete the task with the index input by the user. "	<< "\r\n"
+			<< "1. delete 000001" << "\r\n"
+			<< "the function will then delete the task with the 000001 index in the task list. "	<< "\r\n"
 			<< "\r\n";
 	}
 	else if (desireCommand == "edit"){
 
 		oss << "To use the edit function, it should be input in the following format: "									<< "\r\n"
 			<< "edit <task index> <edit target> <edit informations>"														<< "\r\n"
-			<< "1. edit 1111 des physics homework"																		<< "\r\n"
-			<< "2. edit 1111 date 13 march"																				<< "\r\n"
-			<< "3. edit 1111 time 2pm"																					<< "\r\n"
-			<< "4. edit 1111 prior low" << "\r\n"
+			<< "1. edit 000001 des physics homework"																		<< "\r\n"
+			<< "2. edit 000002 date 13 march"																				<< "\r\n"
+			<< "3. edit 000003 time 2pm"																					<< "\r\n"
+			<< "4. edit 000004 prior low" << "\r\n"
 			<< "the function will edit the task specified with the task index and then modified the categories of the task accordingly."
 			<< "\r\n";
 	}
