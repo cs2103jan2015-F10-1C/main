d35ec97132440f806bef2e032bfae57efb8cf0f8
ShouRuyan <shouruyan.mahiroyo@gmail.com>
2015-03-24 21:33:43 +0800

    Push sort class.

---
+++ oop test/ExecuteSort.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Global.h"
+#include "Storage.h"
+#include "UserTask.h"
+#include "ExtDataBase.h"
+#include "Executor.h"
+
+class ExecuteSort : public Executor {
+private:
+	UserTask* _task;
+
+public:
+	ExecuteSort(UserTask*);
+	~ExecuteSort();
+
+	string execute(Storage&, ExtDataBase);
+	string undo();
+	string getTodayDate();
+	string getAllInfoOfOneTask(list<StickyNote>::iterator);
+	string sortTasksByDate(Storage&);
+	void getFutureTasks(vector<list<StickyNote>::iterator>&, Storage&);
+	string sortTasksPriority(Storage&);
+
+	struct compareTwoTasksByDay{
+		inline bool operator()(const list<StickyNote>::iterator lhs, const list<StickyNote>::iterator rhs) {
+			return lhs->getDay() < rhs->getDay();
+		}
+	};
+};
