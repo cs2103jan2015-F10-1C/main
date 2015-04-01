fdb2df5cd6335393071e695c798582af0d32f413
Qiu Junbin <junbin@live.com>
2015-04-01 15:26:20 +0800

    Made amendments to the help class to include the help class

--- oop test/ExecuteHelp.cpp
+++ oop test/ExecuteHelp.cpp
@@ -50,19 +50,26 @@ string ExecuteHelp::execute(Storage& _storage, ExtDataBase extdb, vector<list<St
 			<< "1.3	For the priority of the task, it can be varied according with inputs: " << "\r\n"
 			<< "\r\n"
 			<< "1. -high represents task of high priority." << "\r\n"
-			<< "2. -mid represents task of medium priority." << "\r\n"
-			<< "3. -low represents task of low priority." << "\r\n"
 			<< "\r\n"
 
 			<< "1.4	For details with conflicting terms such as certain days in a week and am / pm, user needs to include a quotation mark" << "\r\n"
 			<< "\r\n"
 			<< "To avoid creating a task on tuesday or at 3pm." << "\r\n"
 			<< "e.g.meet at \"taco tuesday\" or meet at \"cafe 3pm\" or \"high tea\"" << "\r\n"
+			<< "\r\n"
+
+			<< "1.5	Handling inputs that goes missing" << "\r\n"
+			<< "\r\n"
+			<< "If there is an empty inputs for example:" << "\r\n"
+			<< "UNBOUNDED:" << "\r\n"
+			<< "1.[All day event][Status:incomplete]" << "\r\n"
+			<< "This means there is input of conflicting terms and user can undo the task and adding the task with quotation marks around the task descriptions." << "\r\n"
 			<< "\r\n";
 	}
 	else if (desireCommand == "display"){
 
 		oss << "To use DISPLAY, it should be input in the following format: " << "\r\n"
+			<< "\r\n"
 			<< "1. display" << "\r\n"
 			<< "2. display today" << "\r\n"
 			<< "to display task that are scheduled only for today" << "\r\n"
@@ -71,13 +78,11 @@ string ExecuteHelp::execute(Storage& _storage, ExtDataBase extdb, vector<list<St
 			<< "4. display 3 march" << "\r\n"
 			<< "5. display 3/3" << "\r\n"
 			<< "to display task that are scheduled only on a specific date" << "\r\n"
-			<< "6. display high priority" << "\r\n"
-			<< "to display task that are scheduled based on priority" << "\r\n"
-			<< "7. display [day]" << "\r\n"
+			<< "6. display [day]" << "\r\n"
 			<< "to display task of the particular day" << "\r\n"
-			<< "8. display month mar" << "\r\n"
+			<< "7. display month mar" << "\r\n"
 			<< "to display task of march" << "\r\n"
-			<< "9. display month march" << "\r\n"
+			<< "8. display month march" << "\r\n"
 			<< "to display task of march"
 			<< "\r\n"
 			<< "\r\n";
@@ -85,28 +90,55 @@ string ExecuteHelp::execute(Storage& _storage, ExtDataBase extdb, vector<list<St
 	else if (desireCommand == "search"){
 
 		oss << "To use SEARCH, it should be input in the following format: " << "\r\n"
+			<< "\r\n"
 			<< "search <task description>" << "\r\n"
 			<< "the function will then return the tasks with the keywords input by the user. " << "\r\n"
-			<< "please take note the the task indexes (6 number) as they will be used for delete and edit purposes" << "\r\n"
+			<< "please take note of the task indexes as displayed on the user interface as they will be used for delete and edit purposes" << "\r\n"
 			<< "\r\n";
 	}
 	else if (desireCommand == "delete"){
 
 		oss << "To use DELETE, it should be input in the following format: " << "\r\n"
+			<< "\r\n"
 			<< "delete <task index>" << "\r\n"
-			<< "1. delete 000001" << "\r\n"
-			<< "the function will then delete the task with the 000001 index in the task list. " << "\r\n"
+			<< "\r\n"
+			<< "For example:" << "\r\n"
+			<< "if the display shows:" << "\r\n"
+			<< "1.[All day event] meet junyang[Status:incomplete]" << "\r\n"
+			<< "2.[All day event] meet zann[Status:incomplete]" << "\r\n"
+			<< "3.[All day event] meet ruyan[Status:incomplete]" << "\r\n"
+			<< "\r\n"
+			<< "if the user inputs \"delete 1\" " << "\r\n"
+			<< "the function will then delete the task with index 1 in display list which is: " << "\r\n"
+			<< "1.[All day event] meet junyang[Status:incomplete]" << "\r\n"
 			<< "\r\n";
 	}
 	else if (desireCommand == "edit"){
 
 		oss << "To use EDIT, it should be input in the following format: " << "\r\n"
-			<< "edit <task index> <edit target> <edit informations>" << "\r\n"
-			<< "1. edit 000001 des physics homework" << "\r\n"
-			<< "2. edit 000002 date 13 march" << "\r\n"
-			<< "3. edit 000003 time 2pm" << "\r\n"
-			<< "4. edit 000004 prior low" << "\r\n"
-			<< "the function will edit the task specified with the task index and then modified the categories of the task accordingly."
+			<< "edit <task index> <edit informations>" << "\r\n"
+			<< "\r\n"
+			<< "For example:" << "\r\n"
+			<< "if the display shows:" << "\r\n"
+			<< "TODAY: " << "\r\n"
+			<< "1.[21:00 - 22 : 00] meet zann[Status:incomplete]" << "\r\n"
+			<< "\r\n"
+			<< "The user can input the following to edit the task: " << "\r\n"
+			<< "1. edit 1 meet derrick tomorrow 9pm " << "\r\n"
+			<< "\r\n"
+			<< "The edited task will be as follows: " << "\r\n"
+			<< "TOMORROW:" << "\r\n"
+			<< "1.[21:00 - 22 : 00] meet derrick[Status:incomplete]" << "\r\n"
+			<< "the function will edit the task specified with the task index and then modified the information of the task accordingly." << "\r\n"
+			<< "\r\n"
+			<< "To change task from bounded to unbounded: " << "\r\n"
+			<< "If the screen displays this: " << "\r\n"
+			<< "TOMORROW:" << "\r\n"
+			<< "1.[21:00 - 22 : 00] meet derrick[Status:incomplete]" << "\r\n"
+			<< "edit 1 99/99 99:99 " << "\r\n"
+			<< "This will change the task to unbounded and the screen will appear:  " << "\r\n"
+			<< " UNBOUNDED:" << "\r\n"
+			<< "1.[All day event] meet derrick" << "\r\n"
 			<< "\r\n";
 	}
 	else if (desireCommand == "directory"){
@@ -115,25 +147,56 @@ string ExecuteHelp::execute(Storage& _storage, ExtDataBase extdb, vector<list<St
 			<< "1. directory D:\\savefile.txt"
 			<< "\r\n";
 	}
-	else if (desireCommand == "sort"){
+	/* else if (desireCommand == "sort"){
 		oss << "To use SORT, it should be input in the following format: " << "\r\n"
 			<< "sort by <criteria>" << "\r\n"
 			<< "1. sort by date" << "\r\n"
 			<< "2. sort by priority"
 			<< "\r\n";
-	}
+	} */
+
+	//sort is still undercontruction, the user will already be seeing the sorted form of the tasks.
 	else if (desireCommand == "mark"){
 		oss << "To use MARK, it should be input in the following format: " << "\r\n"
-			<< "mark <index number>" << "\r\n"
+			<< "\r\n"
+			<< "For example:" << "\r\n"
+			<< "if the display shows:" << "\r\n"
+			<< "TODAY: " << "\r\n"
+			<< "1.[21:00 - 22 : 00] meet zann[Status:incomplete]" << "\r\n"
+			<< "\r\n"
+			<< "To signify Task completion: " << "\r\n"
+			<< "user can input in the following format: mark <index number>" << "\r\n"
 			<< "Exampe of using this command: " << "\r\n"
-			<< "1. mark 020300" << "\r\n"
+			<< "1. mark 1" << "\r\n"
+			<< "\r\n"
 			<< "Executing this command will change the status of the task to done." << "\r\n"
+			<< "Task will be shifted to the archive upon this command and user will no longer see it in the display box." << "\r\n"
+			<< "\r\n";
+	}
+	else if (desireCommand == "undo"){
+		oss << "To use UNDO, it should be input in the following format: " << "\r\n"
+			<< "This command reverts the programme back to its previous state." << "\r\n"
+			<< "\r\n"
+			<< "For example:" << "\r\n"
+			<< "if the previous action by an user is \"add do homework today 9pm\" " << "\r\n"
+			<< "resulting which the display shows:" << "\r\n"
+			<< "TODAY: " << "\r\n"
+			<< "1.[21:00 - 22 : 00] do homework[Status:incomplete]" << "\r\n"
+			<< "\r\n"
+			<< "To execute undo for this addition of task: " << "\r\n"
+			<< "user can input in the following format: undo" << "\r\n"
+			<< "Exampe of using this command: " << "\r\n"
+			<< "1. undo" << "\r\n"
+			<< "Executing this command will remove the task added previously." << "\r\n"
+			<< "\r\n"
+			<< "The same applies for mark, edit,delete and change of directory." << "\r\n"
+			<< "Nothing will happen if you undo other commands.(i.e search,sort and display) " << "\r\n"
+			<< "User will be able to keep using UNDO command till his/her very first action" << "\r\n"
 			<< "\r\n";
 	}
 	else{
 		return MESSAGE_INVALID_HELP;
 	}
-
 	successful = true;
 	return oss.str();
 }
