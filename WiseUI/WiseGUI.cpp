7fe034f60e7dbe7dcbfea8e68d3b0840b494bdd9
ShouRuyan <shouruyan.mahiroyo@gmail.com>
2015-04-07 06:37:30 +0800

    1. Modification to GUI. 2. Hot keys are implemented. 3. Messages are shown when minimizing the window to system tray.

--- WiseUI/WiseGUI.cpp
+++ WiseUI/WiseGUI.cpp
@@ -5,6 +5,7 @@ using namespace System::Windows::Forms;
 [STAThread]
 
 int main(array<String^>^ args) {
+
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
