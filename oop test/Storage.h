443bde7f21cb474a10272bc4ca11dad79e51298a
Qiu Junbin <junbin@live.com>
2015-04-07 03:28:19 +0800

    Refactored:
    Storage class
    Standardise class.
    
    Checked on:
    
    UserTask Class
    StickyNote Class
    Parser Class
    Log class
    ExternalDataBase Class
    
    Pending:
    Logic Class
    HandleInput Class

--- oop test/Storage.h
+++ oop test/Storage.h
@@ -15,6 +15,15 @@ public:
 	int getSize();
 	int getSameDateCount(string);
 	bool noRepeatIndexCount(string);
+	bool checkClashExist(list<StickyNote>::iterator, list<StickyNote>::iterator);
+	bool checkInitialCondition(list<StickyNote>::iterator, list<StickyNote>::iterator);
+	bool checkTimeClash(list<StickyNote>::iterator iter, list<StickyNote>::iterator);
+	bool checkNoteClashExist(list<StickyNote>::iterator iter, StickyNote& note);
+	bool checkNoteInitialCondition(list<StickyNote>::iterator, StickyNote&);
+	bool checkNoteTimeClash(list<StickyNote>::iterator, StickyNote&);
+	bool checkIterClashExists(list<StickyNote>::iterator, list<StickyNote>::iterator);
+	bool checkIterInitialCondition(list<StickyNote>::iterator, list<StickyNote>::iterator);
+	bool checkIterTimeClash(list<StickyNote>::iterator, list<StickyNote>::iterator);
 	string oneTaskInfoTypeOne(list<StickyNote>::iterator);
 	string oneTaskInfoTypeTwo(list<StickyNote>::iterator);
 	void findClashes();
